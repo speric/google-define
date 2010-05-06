61a6f2b16957fcff13a76c34ea9c5869e7b2dbd4
speric <eric@prudentiadigital.com>
2010-05-06 09:42:22 -0400

    Clean up Google URL

--- lib/google_define.rb
+++ lib/google_define.rb
@@ -3,7 +3,7 @@ require 'open-uri'
 
 class GoogleDefine
   def self.define(word)
-    doc = Hpricot(open("http://www.google.com/search?hl=en&q=define%3A#{word}&btnG=Google+Search"))
+    doc = Hpricot(open("http://www.google.com/search?hl=en&q=define%3A#{word}"))
     raw_elements = doc.search("//ul[@type='disc']").search("//li")
     raw_elements.search("a").remove   #remove <a> and </a>
     raw_elements.search("p").remove   #remove <p> and </p>
